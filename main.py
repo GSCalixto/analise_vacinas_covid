cd8f3c2b9fc7e2e9b38d15106491db45a26850ce
Gabriel Calixto <gabriel_calixto.gsc@hotmail.com>
2024-04-28 04:57:37 -0300

    filtrando colunas do dataframe

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+'''
+    Lê o arquivo CSV "dados_vacinas_v1.csv" e retorna apenas as colunas "['estabelecimento_municipio_nome', 'vacina_dataAplicacao', 'vacina_nome']"
+   -proximo passo e enviar esse retorno em forma de mensagem para um producer Kafka-
+'''
+def dataframe_filtrado():    
+    path_csv = r"C:\Users\Pc\Desktop\Programação\Python\analise_vacinas_covid\dados_vacinas_v1.csv"
+
+    dados = pd.read_csv(path_csv, sep=';', usecols=['estabelecimento_municipio_nome', 'vacina_dataAplicacao', 'vacina_nome'], on_bad_lines='warn')
+
+    return dados 
