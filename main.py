a8330e62084fba2a98d00cf4525b7e340b8718e4
THEVOL <gabriel_calixto.gsc@hotmail.com>
2024-06-07 21:55:50 -0300

    alterando estrutura de pastas e modularizando o projeto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+from controller.connection import session
+from models.vacinas import Vacinas
+
 '''
     Lê o arquivo CSV "dados_vacinas_v1.csv" e retorna apenas as colunas "['estabelecimento_municipio_nome', 'vacina_dataAplicacao', 'vacina_nome']"
    -proximo passo e enviar esse retorno em forma de mensagem para um producer Kafka-
@@ -9,4 +12,11 @@ def dataframe_filtrado():
 
     dados = pd.read_csv(path_csv, sep=';', usecols=['estabelecimento_municipio_nome', 'vacina_dataAplicacao', 'vacina_nome'], on_bad_lines='warn')
 
-    return dados 
+    return dados
+
+#SELECT
+data = session.query(Vacinas).all()
+print(data)
+
+session.commit()
+session.close() 
